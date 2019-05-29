5c096c446933291810221c8f8265dcdb5424469e
Turbo <turbo@crazy-factory.co.th>
2019-05-29 17:22:07 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,19 @@ class App extends React.Component<{}, {}> {
                 <div className='block-content'>
                     <div className='col-xs-12'>
                         <article className='module'>
-                            <div className='hero-image-title'>
-                                <h1 className='col-xs-9 visible-xs ucase page-title'>NEW APPLIED NOW</h1>
+                            <div className='hero-content'>
+                                <h1 className='col-md-8 col-sm-7 ucase page-title hidden-xs'>NEW APPLIED NOW</h1>
+                                <div className='subhero-content col-sm-6'>
+                                    <div className='hero-description'>
+                                        <p>Discover how you embrace innovation to drive new value for your organization.
+                                            Explore new insights. See tangible outcomes.</p>
+                                    </div>
+                                    <div className='modal-play'>
+                                        <a className='cta-button' href='/th-en/insights/into-the-new' target='' role='button'>
+                                            <div className='btn btn-primary front ucase'>LEARN MORE</div>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </article>
                     </div>
