a219f3b5417626c26151de2870ae4159107eab18
Turbo <p.petpadriew@gmail.com>
2020-03-25 22:44:18 +0700

    winner

--- src/index.js
+++ src/index.js
@@ -22,11 +22,11 @@ class Board extends React.Component {
     }
 
     mark(i) {
-        if (this.state.squares[i] !== null) {
+        const squares = this.state.squares.slice();
+        if (Board.calculateWinner(squares) || squares[i]) {
             return;
         }
 
-        const squares = this.state.squares.slice();
         squares[i] = this.state.currentTurn;
         this.setState({
             squares: squares,
@@ -44,8 +44,35 @@ class Board extends React.Component {
         );
     }
 
+    static calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+
+        return null;
+    }
+
     render() {
-        const status = 'Next player: ' + this.state.currentTurn;
+        const winner = Board.calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + this.state.currentTurn;
+        }
 
         return (
             <div>
